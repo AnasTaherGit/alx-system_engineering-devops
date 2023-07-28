5824671149feb818a0de42b6083ddff656273f55
Anas TAHER <taheranas24@gmail.com>
2023-07-29 00:57:33 +0200

    Task 3 chmod

--- 0x05-processes_and_signals/3-show_your_bash_pid_made_easy.sh
+++ 0x05-processes_and_signals/3-show_your_bash_pid_made_easy.sh
@@ -1,16 +1,4 @@
 #!/usr/bin/env bash
 # Iterate over all pid directories in /proc
 
-for pid_dir in /proc/[0-9]*; do
-    # If cmdline file exists
-    if [[ -f $pid_dir/cmdline ]]; then
-        # Get the process command line
-        cmdline=$(tr '\0' ' ' < "$pid_dir/cmdline")
-        # If command line contains bash
-        if [[ $cmdline == *"bash"* ]]; then
-            # Extract the pid from the directory name and print it with the command line
-            pid=$(basename "$pid_dir")
-            echo "$pid $cmdline"
-        fi
-    fi
-done
+pgrep -l bash
